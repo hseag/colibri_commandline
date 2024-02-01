a1f7bf759160da5564ec58464546b0bb814a3802
Daniel Zwirner <daniel.zwirner@hseag.com>
2024-02-01 09:18:00 +0100

    Fixed some typos

--- main.c
+++ main.c
@@ -21,25 +21,25 @@ void help(int argcCmd, char **argvCmd)
 {
 	if(argvCmd == NULL)
 	{
-	  	fprintf(stdout, "Usage: colibri [OPTIONS] COMMAND [ARGUMENTS]\n");
-	  	fprintf(stdout, "Commands:\n");
-	  	fprintf(stdout, "  get INDEX           : get a value from the device\n");
-	  	fprintf(stdout, "  set INDEX VALUE     : set a value in the device\n");
-	  	fprintf(stdout, "  levelling           : prepares the module for a measurment\n");
-	  	fprintf(stdout, "  save                : save the last measurement(s)\n");
-	  	fprintf(stdout, "  calculate           : caluclate od's or concentrations\n");
-	  	fprintf(stdout, "  measure             : starts a measurement and return the values\n");
-	  	fprintf(stdout, "  version             : return the CLI and DLL version\n");
-	  	fprintf(stdout, "  selftest            : executes an internal selftest\n");
-	  	fprintf(stdout, "  fwupdate FILE       : loads a new firmware\n");
-	  	fprintf(stdout, "  command COMMAND     : executes a command e.g colibri.exe command \"V 0\" returns the value at index 0\n");
-		fprintf(stdout, "  help COMMAND        : Prints a detailed help\n");
-	  	fprintf(stdout, "\n");
-	  	fprintf(stdout, "Options:\n");
-	  	fprintf(stdout, "  --verbose           : prints debug info\n");
-	  	fprintf(stdout, "  --help -h           : show this help and exit\n");
-	  	fprintf(stdout, "  --device            : use the given device, if omitted the CLI searchs for a device\n");
-	  	fprintf(stdout, "  --use-checksum      : use the protocol with a checksum\n");
+			fprintf(stdout, "Usage: colibri [OPTIONS] COMMAND [ARGUMENTS]\n");
+			fprintf(stdout, "Commands:\n");
+			fprintf(stdout, "  get INDEX           : get a value from the device\n");
+			fprintf(stdout, "  set INDEX VALUE     : set a value in the device\n");
+			fprintf(stdout, "  levelling           : prepares the module for a measurment\n");
+			fprintf(stdout, "  save                : save the last measurement(s)\n");
+			fprintf(stdout, "  calculate           : caluclate od's or concentrations\n");
+			fprintf(stdout, "  measure             : starts a measurement and return the values\n");
+			fprintf(stdout, "  version             : return the CLI and DLL version\n");
+			fprintf(stdout, "  selftest            : executes an internal selftest\n");
+			fprintf(stdout, "  fwupdate FILE       : loads a new firmware\n");
+			fprintf(stdout, "  command COMMAND     : executes a command e.g colibri.exe command \"V 0\" returns the value at index 0\n");
+			fprintf(stdout, "  help COMMAND        : Prints a detailed help\n");
+			fprintf(stdout, "\n");
+			fprintf(stdout, "Options:\n");
+			fprintf(stdout, "  --verbose           : prints debug info\n");
+			fprintf(stdout, "  --help -h           : show this help and exit\n");
+			fprintf(stdout, "  --device            : use the given device, if omitted the CLI searchs for a device\n");
+			fprintf(stdout, "  --use-checksum      : use the protocol with a checksum\n");
 	}
 	else
 	{
@@ -47,58 +47,58 @@ void help(int argcCmd, char **argvCmd)
 		{
 			if(strcmp(argvCmd[1], "get") == 0)
 			{
-				fprintf(stdout, "Usage: colibri get INDEX]\n");
+				fprintf(stdout, "Usage: colibri get INDEX\n");
 				fprintf(stdout, "  Get a value from the device\n");
 				fprintf(stdout, "INDEX:\n");
-				fprintf(stdout, "   0: Firmware version:\n");
-				fprintf(stdout, "   1: Serial number:\n");
+				fprintf(stdout, "   0: Firmware version\n");
+				fprintf(stdout, "   1: Serial number\n");
 				fprintf(stdout, "   2: Hardware type\n");
-				fprintf(stdout, "  10: Number of internal stored last measurements:\n");
-				fprintf(stdout, "  23: Max current in [uA] for the 230nm LED:\n");
-				fprintf(stdout, "  33: Max current in [uA] for the 260nm LED:\n");
-				fprintf(stdout, "  43: Max current in [uA] for the 280nm LED:\n");
-				fprintf(stdout, "  53: Max current in [uA] for the 340nm LED:\n");
-				fprintf(stdout, "  60: Sample amplification factor nominal 1.1:\n");
-				fprintf(stdout, "  61: Sample amplification factor nominal 11.0:\n");
-				fprintf(stdout, "  62: Sample amplification factor nominal 111.0:\n");
-				fprintf(stdout, "  63: Reference amplification factor nominal 1.1:\n");
-				fprintf(stdout, "  64: Reference amplification factor nominal 11.0:\n");
-				fprintf(stdout, "  65: Reference amplification factor nominal 111.0:\n");				
-				fprintf(stdout, "  80: Levelling voltage in [uV] for the 230n LED:\n");
-				fprintf(stdout, "  81: Levelling voltage in [uV] for the 260n LED:\n");
-				fprintf(stdout, "  82: Levelling voltage in [uV] for the 280n LED:\n");
-				fprintf(stdout, "  83: Levelling voltage in [uV] for the 340n LED:\n");
+				fprintf(stdout, "  10: Number of internal stored last measurements\n");
+				fprintf(stdout, "  23: Max current in [uA] for the 230nm LED\n");
+				fprintf(stdout, "  33: Max current in [uA] for the 260nm LED\n");
+				fprintf(stdout, "  43: Max current in [uA] for the 280nm LED\n");
+				fprintf(stdout, "  53: Max current in [uA] for the 340nm LED\n");
+				fprintf(stdout, "  60: Sample amplification factor nominal 1.1\n");
+				fprintf(stdout, "  61: Sample amplification factor nominal 11.0\n");
+				fprintf(stdout, "  62: Sample amplification factor nominal 111.0\n");
+				fprintf(stdout, "  63: Reference amplification factor nominal 1.1\n");
+				fprintf(stdout, "  64: Reference amplification factor nominal 11.0\n");
+				fprintf(stdout, "  65: Reference amplification factor nominal 111.0\n");				
+				fprintf(stdout, "  80: Levelling voltage in [uV] for the 230n LED\n");
+				fprintf(stdout, "  81: Levelling voltage in [uV] for the 260n LED\n");
+				fprintf(stdout, "  82: Levelling voltage in [uV] for the 280n LED\n");
+				fprintf(stdout, "  83: Levelling voltage in [uV] for the 340n LED\n");
 			}
 			else if(strcmp(argvCmd[1], "set") == 0)
 			{
-				fprintf(stdout, "Usage: colibri set INDEX VALUE]\n");
+				fprintf(stdout, "Usage: colibri set INDEX VALUE\n");
 				fprintf(stdout, "  Set a value in the device\n");
 				fprintf(stdout, "WARNING:\n");
 				fprintf(stdout, "  Changing a value can damage the device or lead to incorrect results!\n");
 				fprintf(stdout, "INDEX:\n");
-				fprintf(stdout, "   1: Serial number:\n");
+				fprintf(stdout, "   1: Serial number\n");
 				fprintf(stdout, "   2: Hardware type\n");
-				fprintf(stdout, "  23: Max current in [uA] for the 230nm LED:\n");
-				fprintf(stdout, "  33: Max current in [uA] for the 260nm LED:\n");
-				fprintf(stdout, "  43: Max current in [uA] for the 280nm LED:\n");
-				fprintf(stdout, "  53: Max current in [uA] for the 340nm LED:\n");
-				fprintf(stdout, "  60: Sample amplification factor nominal 1.1:\n");
-				fprintf(stdout, "  61: Sample amplification factor nominal 11.0:\n");
-				fprintf(stdout, "  62: Sample amplification factor nominal 111.0:\n");
-				fprintf(stdout, "  63: Reference amplification factor nominal 1.1:\n");
-				fprintf(stdout, "  64: Reference amplification factor nominal 11.0:\n");
-				fprintf(stdout, "  65: Reference amplification factor nominal 111.0:\n");				
-				fprintf(stdout, "  80: Levelling voltage in [uV] for the 230n LED:\n");
-				fprintf(stdout, "  81: Levelling voltage in [uV] for the 260n LED:\n");
-				fprintf(stdout, "  82: Levelling voltage in [uV] for the 280n LED:\n");
-				fprintf(stdout, "  83: Levelling voltage in [uV] for the 340n LED:\n");
+				fprintf(stdout, "  23: Max current in [uA] for the 230nm LED\n");
+				fprintf(stdout, "  33: Max current in [uA] for the 260nm LED\n");
+				fprintf(stdout, "  43: Max current in [uA] for the 280nm LED\n");
+				fprintf(stdout, "  53: Max current in [uA] for the 340nm LED\n");
+				fprintf(stdout, "  60: Sample amplification factor nominal 1.1\n");
+				fprintf(stdout, "  61: Sample amplification factor nominal 11.0\n");
+				fprintf(stdout, "  62: Sample amplification factor nominal 111.0\n");
+				fprintf(stdout, "  63: Reference amplification factor nominal 1.1\n");
+				fprintf(stdout, "  64: Reference amplification factor nominal 11.0\n");
+				fprintf(stdout, "  65: Reference amplification factor nominal 111.0\n");				
+				fprintf(stdout, "  80: Levelling voltage in [uV] for the 230n LED\n");
+				fprintf(stdout, "  81: Levelling voltage in [uV] for the 260n LED\n");
+				fprintf(stdout, "  82: Levelling voltage in [uV] for the 280n LED\n");
+				fprintf(stdout, "  83: Levelling voltage in [uV] for the 340n LED\n");
 			}
 			else if(strcmp(argvCmd[1], "levelling") == 0)
 			{
 				fprintf(stdout, "Usage: colibri levelling\n");
 				fprintf(stdout, "  The levelling commands sets the LED currents and amplifications so that the levelling voltage is reached.\n");
 				fprintf(stdout, "  The values determined are internally stored and printed to stdout.\n");
-				fprintf(stdout, "  The firmware has an internal storage for up to then measurements. The command levelling clears this storage.\n");
+				fprintf(stdout, "  The firmware has an internal storage for up to ten measurements. The command levelling clears this storage.\n");
 				fprintf(stdout, "Usage: colibri levelling 0\n");
 				fprintf(stdout, "  The last values are printed to stdout.\n");
 				fprintf(stdout, "Output:\n");
